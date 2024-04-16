e3a7994e78f91c68376146dbe4c47196bec48d72
xavier <villax989@icloud.com>
2024-04-16 17:58:41 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+document.querySelector("main#main").remove()
+
+const newHeader = document.createElement('h1')
+
+newHeader.setAttribute("id","victory")
+
+newHeader.textContent = ("is the champion")
+
